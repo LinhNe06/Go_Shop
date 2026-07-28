06e9cbe42d13637f0413cb93ebaeaf32a11253f4
NXL <linhdenday16206@gmail.com>
2026-07-28 17:33:51 +0700

    Demo failed CI

--- backend/main_test.go
+++ backend/main_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestSanitizeFilename(t *testing.T) {
 	got := sanitizeFilename("My Photo (1).PNG")
-	if want := "my-photo--1-.png"; got != want {
-		t.Errorf("sanitizeFilename() = %q, want %q", got, want)
-	}
-	// if want := "test-that-must-fail"; got != want {
+	// if want := "my-photo--1-.png"; got != want {
 	// 	t.Errorf("sanitizeFilename() = %q, want %q", got, want)
 	// }
+	if want := "test-that-must-fail"; got != want {
+		t.Errorf("sanitizeFilename() = %q, want %q", got, want)
+	}
 }
 
 func TestSanitizeFilenameBlocksPathTraversal(t *testing.T) {
